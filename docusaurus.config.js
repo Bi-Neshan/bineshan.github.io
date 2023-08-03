c86eb6068fda7012974ddb179ce07afac75af977
Ahmad <ahmad.motamedi@gmail.com>
2023-08-03 16:18:24 +0330

    fix repository name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Bi-Neshan', // Usually your GitHub org/user name.
-  projectName: 'bineshan.github.io', // Usually your repo name.
+  projectName: 'bineshan.net', // Usually your repo name.
   deploymentBranch: 'main',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
