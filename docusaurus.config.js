2ff8959fcb7ac4ffea5545639788819fc005c03b
Ahmad <ahmad.motamedi@gmail.com>
2023-08-05 01:53:12 +0330

    add download page and edit home page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,11 +72,7 @@ const config = {
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
-        },
-        gtag: {
-          trackingID: 'G-999X9XX9XX',
-          anonymizeIP: true,
-        },
+        }
       }), 
     ],
   ],
